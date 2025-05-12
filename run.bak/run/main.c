77a74934417d2d24d116edd84e332c9b8a915ee5
Merlin Brandt <merlin.brandt@hotmail.de>
2025-05-12 15:53:03 +0100

    untitled

--- run.bak/run/main.c
+++ run.bak/run/main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-char *lastcmd = "gitupdate ";
+char *lastcmd = "cd ..";
 int syssh = 0;
 char *workdir = "/data/data/com.termux/files/home/prj/bytesh//run";
 int main(int argc, char **argv) {
 MAIN_BEGIN
-
+gitupdate 
 MAIN_END}
