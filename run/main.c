dab74912f7c85ccdb681b5b1d4f66b81c1cad274
Merlin Brandt <merlin.brandt@hotmail.de>
2025-06-23 14:32:25 +0000

    untitled

--- run/main.c
+++ run/main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-char *lastcmd = "sh, \"git status\", endsh ";
+char *lastcmd = "sh, \"git add .gitignore\", endsh ";
 int syssh = 0;
 char *rootworkdir = "/data/data/com.termux/files/home/prj/bytesh//run";int isfirstcmd = 0;
 int main(int argc, char **argv) {
